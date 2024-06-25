600465c24cbbcf80230ac3e0e941bf0d28d5ba17
javaexpresschannel <javaexpresschannel@gmail.com>
2024-06-25 21:53:18 +0530

    First commit

---
+++ 02-SB-web-actuators/src/main/java/com/javaexpress/beans/Address.java
@@ -0,0 +1,11 @@
+package com.javaexpress.beans;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Address {
+
+	public Address() {
+		System.out.println("Address Defualt Constructor");
+	}
+}
---
+++ 02-SB-web-actuators/src/main/java/in/javaexpress/Department.java
@@ -0,0 +1,11 @@
+package in.javaexpress;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Department {
+
+	public Department() {
+		System.out.println("Department constuctor - in.javaexpress");
+	}
+}
